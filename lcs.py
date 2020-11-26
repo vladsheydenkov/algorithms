7649edcf88c41e1e7d3d7077ba243f0c7dc31f19
vlad sheydenkov <vladsheydenkov@gmail.com>
2020-11-26 14:28:53 +0300

    add lcs

---
+++ lcs.py
@@ -0,0 +1,15 @@
+def lcs(a, b):
+    F = [[0] * (len(b) + 1) for i in range(len(a) + 1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i-1] == b[j-1]:
+                F[i][j] = 1 + F[i-1][j-1]
+            else:
+                F[i][j] = max(F[i-1][j], F[i][j-1])
+    for i in F:
+        print(i)
+
+
+# magic numbers, names
+
+
